b37ccddd73b43023f0a7ae63c55c37563265ae1c
Тот самый Дмитрий <151468115+64kda@users.noreply.github.com>
2024-04-02 00:05:10 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+import tqdm
+import time
+
+@pytest.mark.xfail(reason="Wait for fix bug")
+
+users = []
+
+for i in tqdm.trange(1, 10001):
+    user = {}
+    user['id'] = i
+    user['name'] = f'Name_{i}'
+    user['age'] = random.randint(25, 60)
+    user['email'] = f'sample_{i}@mail.ru'
+    users.append(user)
+    time.sleep(0.01)
+
+print('Ok')
